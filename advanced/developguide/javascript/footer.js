84ec96568294188843950cc45f6e3d48259b5967
Liupo Wu <wuliupo@163.com>
2014-05-07 13:42:51 +0800

    develop guide from http://faq.comsenz.com/viewnews-464

---
+++ advanced/developguide/javascript/footer.js
@@ -0,0 +1,3 @@
+document.write('</div><div class="footer">');
+document.write('版权所有 &copy; 2001 - 2008 <a href="http://www.comsenz.com/" target="_blank">康盛创想(北京)科技有限公司 Comsenz Inc</a>');
+document.write('</div></div>');
